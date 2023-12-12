bb66651d51457b13d4ec64de0c07c529a82b0f97
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-12-12 10:20:37 +0000

    new commit

--- Week1.py
+++ Week1.py
@@ -1,1 +1,9 @@
-print('Hello world')
+# Python data types
+# Set data types
+
+# basket = {'Apples','Orange','Banana','Grape', 'Pear'}
+# print (basket)
+
+a = set('abracadabra')
+a.add('z')
+print(a)
