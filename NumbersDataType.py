57bb501ab43acab4710a7fbdb3a06ebbdb21e4c9
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-12-12 10:58:02 +0000

    new commit

---
+++ NumbersDataType.py
@@ -0,0 +1,8 @@
+#  Numbers data type
+# Numbers have four types in Python. Int, float, complex, and long.
+int_num = 10 #int value
+float_num = 10.2 #float value
+complex_num = 3.14j #complex value
+# long_num = 1234567L #long value
+
+print(int_num, float_num, complex_num)
