3b64df9ad008d942757f6b61924d2f77f75b2888
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-12-12 10:32:33 +0000

    commit

--- Week1.py
+++ Week1.py
@@ -4,6 +4,13 @@
 # basket = {'Apples','Orange','Banana','Grape', 'Pear'}
 # print (basket)
 
-a = set('abracadabra')
-a.add('z')
-print(a)
+# a = set('abracadabra')
+# a.add('z')
+# print(a)
+
+#  Frozen Sets - They are immutable and new elements cannot added after its defined.
+b = frozenset(['a','b','c','d','e'])
+print(b)
+city = frozenset(['Cardiff','Newport','Pontypridd','Pontyclun'])
+
+print(city)
