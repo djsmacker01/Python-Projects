d916859d179a66ff60d95d86d06c42b6fc04fd4b
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-12-19 13:31:21 +0000

    New commit

--- DateTime_func.py
+++ DateTime_func.py
@@ -1,6 +1,6 @@
 # When Passing a date and time object
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta,timezone
+
 
 # dt = datetime.datetime.strptime("2023-04-15T08:27:18-0500", "%Y-%m-%dT%H:%M:%S%z")
 # print(dt)
@@ -12,3 +12,10 @@ print("Current Date and Time:", current_datetime)
 
 new_datetime = current_datetime + timedelta(days=1)
 print("New Date and Time:", new_datetime)
+
+now = datetime.now()
+then = datetime(2023,5,23)
+
+delta = now - then
+
+print(delta.days)
