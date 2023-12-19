032b577e5768f27d6ea98e414d7191b5e1587fd8
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-12-19 14:02:57 +0000

    new commit

---
+++ HeadTailGame.py
@@ -0,0 +1,10 @@
+coin_flip = input("Did the coin land on heads or tails? ")
+
+if coin_flip == "heads":
+    print("The coin land on heads! You Win!")
+elif coin_flip == "tails":
+    print("The coin land on tails! You lose!")
+
+else:
+    print("Please enter a valid answer")    
+print(coin_flip)    
