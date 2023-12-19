be6c5dd35d8a46f806b69bad8c51b3f840aed4ec
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-12-19 13:48:50 +0000

    new commit

--- TypeCasting.py
+++ TypeCasting.py
@@ -1,3 +1,26 @@
 float_number = 3.14
 integer_number = int(float_number)
 print(integer_number)  # Output: 3
+
+number = 12
+print(type(number)) # Output:
+
+number = 42
+string_number = str(number)
+print(string_number)  # Output: '42'
+
+zero = 0
+non_zero = 42
+bool_zero = bool(zero)
+bool_non_zero = bool(non_zero)
+print(bool_zero)      # Output: False
+print(bool_non_zero)  # Output: True
+
+# Input () /Type casting
+firstName = input('What is your name? ')
+age = int(input('How old are you? '))
+
+print(f"My name is {firstName} and in 10 years i will be {age + 10} years old")
+
+#
+
