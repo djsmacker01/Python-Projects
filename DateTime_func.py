0deed8b07088b9ea815e6274aacefc885a5fd6ab
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-12-19 12:21:46 +0000

    timedelta DateTime_func

--- DateTime_func.py
+++ DateTime_func.py
@@ -1,7 +1,14 @@
 # When Passing a date and time object
-import datetime
-dt = datetime.datetime.strptime("2023-04-15T08:27:18-0500", "%Y-%m-%dT%H:%M:%S%z")
-print(dt)
+from datetime import datetime
+from datetime import timedelta
+
+# dt = datetime.datetime.strptime("2023-04-15T08:27:18-0500", "%Y-%m-%dT%H:%M:%S%z")
+# print(dt)
+# Add 1 day to the current date
+
 
 current_datetime = datetime.now()
 print("Current Date and Time:", current_datetime)
+
+new_datetime = current_datetime + timedelta(days=1)
+print("New Date and Time:", new_datetime)
