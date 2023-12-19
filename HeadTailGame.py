49c68fc99545766592920a3187b46c6b5f491c10
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-12-19 16:35:10 +0000

    modified

--- HeadTailGame.py
+++ HeadTailGame.py
@@ -1,10 +1,37 @@
-coin_flip = input("Did the coin land on heads or tails? ")
+import random
 
-if coin_flip == "heads":
-    print("The coin land on heads! You Win!")
-elif coin_flip == "tails":
-    print("The coin land on tails! You lose!")
+# coin_flip = input("Did the coin land on heads or tails? ")
+
+# if coin_flip == "heads":
+#     print("The coin land on heads! You Win!")
+# elif coin_flip == "tails":
+#     print("The coin land on tails! You lose!")
+
+# else:
+#     print("Please enter a valid answer")    
+# print(coin_flip)    
+
+# using random function
+print("Welcome to the head and tail Game")
+
+user_Player = input("Enter your guess (heads or tails): ").lower()
+
+if user_Player not in ['heads','tails']:  
+    print("Invalid input. Please enter 'heads' or 'tails'. ")
 
 else:
-    print("Please enter a valid answer")    
-print(coin_flip)    
+    # Flip the coin
+    coin_result = random.choice(['heads', 'tails'])   
+
+    # display the result
+    print(f"The coin landed on : {coin_result.capitalize()}")
+
+    #  check if user guess correctly
+
+    if user_Player == coin_result:
+        print("Congratulations! you guessed correctly")
+
+    else:
+        print("Oops! You guessed incorrectly")   
+
+
