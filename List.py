6e97841a47d191cc84794bfa836b18842a7146de
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-12-26 20:09:54 +0000

    commit

--- List.py
+++ List.py
@@ -1,4 +1,15 @@
-myList = [2,4,"Matthew", True, "John"]
+# myList = [2,4,"Matthew", True, "John"]
 
-print(f"Here is a list Object {myList}" )
-print(myList[2])
+# print(f"Here is a list Object {myList}" )
+# print(myList[2])
+
+a = [1,2,3,4]
+b = [5,6,7,8,9]
+
+a.extend(b)
+a.join(b)
+print(a)
+
+fruits = ['apple', 'banana', 'cherry']
+index = fruits.index('banana')
+print(index)  # Output: 1
