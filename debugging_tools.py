3d8cae1284da9a55c9e6b3fbf1adf6dd304f7270
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-09 18:05:13 +0000

    new commit message

---
+++ debugging_tools.py
@@ -0,0 +1,8 @@
+def profile(firstname,lastname):
+    full_name = f"{firstname} {lastname}"
+    print("Full Name :",full_name)
+
+
+firstname = 'Smith'
+lastname = 'Mathew'
+profile(firstname,lastname)
