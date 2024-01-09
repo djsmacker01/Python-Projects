ed69dafa39dc45a71f0b36077b1078b63c8c2d9e
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-09 18:22:08 +0000

    new commit

--- debugging_tools.py
+++ debugging_tools.py
@@ -7,7 +7,19 @@
 # lastname = 'Mathew'
 # profile(firstname,lastname)
 
-def profile(firstname,lastname):
-    return f"{firstname} {lastname}"
+# def profile(firstname,lastname):
+#     return f"{firstname} {lastname}"
 
-print(profile('smaith', 'Jackson'))   
+# print(profile('smith', 'Jackson'))   
+
+num1 = 0
+num2 = 10
+
+while num1 <= num2:
+    num1 += 1
+    num2 *= 1
+
+    if num1 == 5:
+        num2 *=2
+        continue
+    print('Task Completed')
