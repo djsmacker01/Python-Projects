e2b92287f2217cd4b2226e502cbdf421e5d22d4c
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-23 20:32:43 +0000

    new commit message

--- indexFun.py
+++ indexFun.py
@@ -1,20 +1,20 @@
-def new_job(data):
-    while True:
-        '''
-        A function that will edit the value of the job key in the json file
-        '''
-        try:
-            entry = int(input("What is the id of the friend who got new job?: "))
-            # int(entry),"The response should be enter an integer value"
-        except:
-            print(f"Please enter an integer value")
-        else:
-            entry = int(entry)
-            break
+# def new_job(data):
+#     while True:
+#         '''
+#         A function that will edit the value of the job key in the json file
+#         '''
+#         try:
+#             entry = int(input("What is the id of the friend who got new job?: "))
+#             # int(entry),"The response should be enter an integer value"
+#         except:
+#             print(f"Please enter an integer value")
+#         else:
+#             entry = int(entry)
+#             break
 
 
 
-    data[entry - 1]["job"] = input("What is the new job title?")
+#     data[entry - 1]["job"] = input("What is the new job title?")
 
 # def new_job(data):
 #     while True:
@@ -47,9 +47,65 @@ def new_job(data):
 #     except Exception as e:
 #         print(f"Error: {e}")
 
+# def birthday(data):
+#     while True:
+#         try:
+#             entry = int(input("what is the id of the friend who had a birthday?: "))  
+#         except:
+#             print("")   
+
+
+def new_job(data):
+    '''
+    A function that will edit the value of the "job" key in the json file
+    :param data: The json file you have imported
+    :return: NA
+    '''
+    while True:
+        try:
+            entry = int(input("What is the id of the friend who got a new job?: "))
+        except:
+            print("Please enter an integer value")
+        else:
+            entry = int(entry)
+            break
+
+    data[entry - 1]["job"] = input("What is the new job title? : ")
+
+# Create a new function called birthday, this function will automatically increment the age value of the
+# selected friend by one
+
+
 def birthday(data):
     while True:
         try:
-            entry = int(input("what is the id of the friend who had a birthday?: "))  
+            entry = int(input("What is the id of the friend who had a birthday?: "))
         except:
-            print("")   
+            print("Please enter an integer value")
+        else:
+            entry = int(entry)
+            break
+
+    while True:
+        try:
+            entry <= len(data)
+        except:
+            print(f"Please enter a value equal to or lower then{len(data)}")
+        else:
+            entry = int(entry)
+            break
+    data[entry - 1]["age"] += 1
+
+def new_friend(data):
+    id = data[-1]["friend_id"] + 1
+    name = input("Please enter your new friends name: ")
+    age = int(input("Please enter your new friends age: "))
+    score = float(input("Please enter your new friends friend score: "))
+    job = input("Please enter your new friends job: ")
+    data.append({
+        "friend_id" : id,
+        "name" : name,
+        "age" : age,
+        "score" : score,
+        "job" : job
+    })
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import json
-import indexFun as pf
+# import json
+# import indexFun as pf
 
-with open("main.json", "r+") as open_json:
-    friends = json.load(open_json)
+# with open("main.json", "r+") as open_json:
+#     friends = json.load(open_json)
 
-# pf.new_job(friends)
-# pf.create_birthday(friends, friend_id = 1)
-choice = input ("Would yo")
-print(friends)  
-with open("main.json", "w") as write_json:
-    edited_json = json.dumps(friends,indent = 5)
-    write_json.write(edited_json)
+# # pf.new_job(friends)
+# # pf.create_birthday(friends, friend_id = 1)
+# choice = input ("Would yo")
+# print(friends)  
+# with open("main.json", "w") as write_json:
+#     edited_json = json.dumps(friends,indent = 5)
+#     write_json.write(edited_json)
 # import json
 
 # # Open the JSON file for reading
@@ -34,3 +34,32 @@ with open("main.json", "w") as write_json:
 
 
 # Print off the two dictionaries separetely using a for loop
+
+
+import json
+import practice_funcs as pf
+
+with open("friends.json", "r+") as open_json:
+    friends = json.load(open_json)
+
+
+choice = input("Would you like to change a job, have a birthday or add a friend? \n enter 'job' or 'birthday' or 'friend': ")
+
+if choice == "job":
+    pf.new_job(friends)
+elif choice == "birthday":
+    pf.birthday(friends)
+elif choice == "friend":
+    pf.new_friend(friends)
+else:
+    print("Please enter a valid answer")
+
+print(friends)
+
+
+pf.new_friend(friends)
+
+
+with open("friends.json", "w") as write_json:
+    edited_json = json.dumps(friends, indent = 4)
+    write_json.write(edited_json)
