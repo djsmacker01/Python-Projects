de48420df072a770d24aef712210035f65309d76
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-28 10:06:13 +0000

    new commit message

--- Debugging Assessment/testing.py
+++ Debugging Assessment/testing.py
@@ -2,5 +2,6 @@ import week10
 
 
 print(week10.addnum(50,89))
+print(week10.num)
 # answer = addnum(50,40)
 # print (week10)
--- Debugging Assessment/week10.py
+++ Debugging Assessment/week10.py
@@ -3,6 +3,8 @@ def addnum(n1, n2):
     return result
 
 
+num = 34
+
 # answer = addnum(20,30)
 # print(answer)    
 # print(week10.path)
