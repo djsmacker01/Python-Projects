b4060cc2421a94c45fd723f0cd76e7461d1f8543
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-01 09:38:41 +0000

    modified

--- Debugging Assessment/testing.py
+++ Debugging Assessment/testing.py
@@ -1,7 +1,11 @@
-import week10
 
 
-print(week10.addnum(50,89))
-print(week10.num)
-# answer = addnum(50,40)
-# print (week10)
+
+
+# import week10
+
+
+# print(week10.addnum(50,89))
+# print(week10.num)
+# # answer = addnum(50,40)
+# # print (week10)
