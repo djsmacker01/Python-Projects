850884a61f2ce038833edefbf31e62d70a0c97fe
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-01 12:00:18 +0000

    new commit

--- Debugging Assessment/main.py
+++ Debugging Assessment/main.py
@@ -1,47 +1,47 @@
-morse_chart = {
-    "a": "",
-    "b": "-...",
-    "c": "-.-.",
-    "d": "-..",
-    "e": "",
-    "f": "..-.",
-    "g": ".-",
-    "h": "....",
-    "i": "",
-    "j": ".---",
-    "k": "-.-",
-    "l": ".-..",
-    "m": "--",
-    "n": "-.",
-    "o": "",
-    "p": ".--.",
-    "q": "--.-",
-    "r": ".-.",
-    "s": "...",
-    "t": "-",
-    "u": "",
-    "v": "...-",
-    "w": ".--",
-    "x": "-..-",
-    "y": "-.--",
-    "z": "--..",
-}
+# morse_chart = {
+#     "a": "",
+#     "b": "-...",
+#     "c": "-.-.",
+#     "d": "-..",
+#     "e": "",
+#     "f": "..-.",
+#     "g": ".-",
+#     "h": "....",
+#     "i": "",
+#     "j": ".---",
+#     "k": "-.-",
+#     "l": ".-..",
+#     "m": "--",
+#     "n": "-.",
+#     "o": "",
+#     "p": ".--.",
+#     "q": "--.-",
+#     "r": ".-.",
+#     "s": "...",
+#     "t": "-",
+#     "u": "",
+#     "v": "...-",
+#     "w": ".--",
+#     "x": "-..-",
+#     "y": "-.--",
+#     "z": "--..",
+# }
 
-def convert_to_morse_code(text):
-    '''
-    Converts English text to morse code
-    :param text: Text to translate
-    :return: A string of morse code
-    '''
-    conversion = ""
-    for i in range(1, len(text)):
-        character = text[i]
-        if character in morse_chart:
-            conversion += morse_chart[character]
-    return conversion
+# def convert_to_morse_code(text):
+#     '''
+#     Converts English text to morse code
+#     :param text: Text to translate
+#     :return: A string of morse code
+#     '''
+#     conversion = ""
+#     for i in range(1, len(text)):
+#         character = text[i]
+#         if character in morse_chart:
+#             conversion += morse_chart[character]
+#     return conversion
 
 
-print(convert_to_morse_code(input("Enter text to convert: ")))
+# print(convert_to_morse_code(input("Enter text to convert: ")))
 
 
 
