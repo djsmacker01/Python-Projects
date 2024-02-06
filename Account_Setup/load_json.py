dc4cb95369f3efe3f653d5f0e00d42e988e696c2
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-06 14:12:36 +0000

    new commit

--- Account_Setup/load_json.py
+++ Account_Setup/load_json.py
@@ -4,3 +4,8 @@ def load_json(file_path):
     """Load JSON data from a file."""
     with open(file_path, "r") as file:
         return json.load(file)
+
+def save_json(file_path, data):
+    """Save JSON data to a file."""
+    with open(file_path, "w") as outfile:
+        json.dump(data, outfile, indent=4)
